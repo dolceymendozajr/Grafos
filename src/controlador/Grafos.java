f86cdc630a6071d7177d39052c8b7141766b8a80
dolceymendozajr <dolceya@uninorte.edu.co>
2017-09-20 10:26:21 -0500

    CrearMatriz and MostrarMatriz methods were created

--- src/controlador/Grafos.java
+++ src/controlador/Grafos.java
@@ -10,30 +10,73 @@ import vista.Vista;
  */
 public class Grafos {
 
+    /*Creacion de la matriz de adyacencia que luego generara el grafo */
+    static int MA[][];
+
     public static void main(String[] args) {
         Vista v = new Vista();
         v.setLocationRelativeTo(null);
         v.setVisible(true);
     }
 
-    public static void CrearGrafo(int v) {
+    /*Método que permite Crear la matriz de Adyacencia*/
+    public static void CrearMatriz(int v) {
+        MA = new int[v][v];
+        boolean sw;
+        int a=-1;
+        int p=-1;
         for (int i = 0; i < v; i++) {
-            boolean sw;
+            System.out.println("Nodo" + i);
             do {
                 try {
-                    String ar = JOptionPane.showInputDialog(null, "Digite el número de aristas para el vertice con valor");
-                    int a = Integer.parseInt(ar);
+                    String ar = JOptionPane.showInputDialog(null, "Digite a que vertice esta conectado el vertice " + i);
+                    a = Integer.parseInt(ar);
+                    String pe = JOptionPane.showInputDialog(null, "Digite el peso de aristas la arista " + i + a);
+                    p = Integer.parseInt(pe);
                     sw = false;
                 } catch (Exception e) {
                     JOptionPane.showMessageDialog(null, "Debe llenar los campos con números enteros", "Error de ingreso", JOptionPane.ERROR_MESSAGE);
                     sw = true;
                 }
             } while (sw);
+            if (!(a==p&&p==-1)) {
+                MA[i][a] = p;
+            }
+        }
+    }
 
-//            do{
-            char id = JOptionPane.showInputDialog("Digite la letra del vertice").charAt(0);
-//            }while(id!=null);
-            Nodo t = new Nodo(id);
+//        char idr = JOptionPane.showInputDialog("Digite la letra del vertice").charAt(0);
+//        root = new Nodo(idr);
+//        v--;
+//        for (int i = 0; i < v; i++) {
+//            boolean sw;
+//            System.out.println("1");
+//            do {
+//                try {
+//                    String ar = JOptionPane.showInputDialog(null, "Digite el número de aristas para el vertice A");
+//                    int a = Integer.parseInt(ar);
+//                    sw = false;
+//                } catch (Exception e) {
+//                    JOptionPane.showMessageDialog(null, "Debe llenar los campos con números enteros", "Error de ingreso", JOptionPane.ERROR_MESSAGE);
+//                    sw = true;
+//                    System.out.println(e);
+//                }
+//            } while (sw);
+//            char id = JOptionPane.showInputDialog("Digite la letra del vertice").charAt(0);
+//            root.addHijo(id);
+//        }
+    
+    /*Método que guardará la matriz de adyacencia en un archivo*/
+    public static void MostrarMatriz(int v) {
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                if(j==v-1){
+                    System.out.print(MA[i][j]);
+                }else{
+                    System.out.print(MA[i][j]+",");
+                }
+            }
+            System.out.println("");
         }
     }
 
